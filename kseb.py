3dbf34e95579a9b034da6cf4a00a157661baac78
vishal8202 <vishalvijayan222@gmail.com>
2022-11-10 11:06:35 +0530

    add

--- kseb.py
+++ kseb.py
@@ -1,37 +1,34 @@
+from tabulate import tabulate
+
 import mysql.connector
-
-mydb = mysql.connector.connect(host = 'localhost' , user = 'root' , password = '' , database = 'ksebdb')
-
-mycursor = mydb.cursor()
-
+try:
+        mydb = mysql.connector.connect(host = 'localhost',user = 'root' ,password = '',database = 'ksebdb')
+except mysql.connector.Error as e:
+        print("MySql error",e)
 mycursor = mydb.cursor()
 while True:
     print("select an option from the menu")
-    print('1 add consumer')
-    print('2 search consumer')
-    print('3 delete consumer')
-    print('4 update consumer')
-    print('5 view all  consumer')
-    print('6 generate bill consumer')
-    print('7 view bill')
-    print('8 exit')
-
-       choice = int(input('Enter an option: '))
-       if(choice==1):
-        print('enter consumer details')
-        elif(choice==2):
-            print('search consumer')   
-         elif(choice==3):
-            print('delete consumer')  
-         elif(choice==4):
-            print('update consumer') 
-         elif(choice==5):
-            print('view all consumer')  
-         elif(choice==6):
-            print('generate bill ') 
-         elif(choice==7):
-            print('view bill')   
-         elif(choice==8):
-         break
-
-        
+    print("""           1. add consumer
+             2. search consumer
+             3. delete  consumer
+             4. update the consumer
+             5. view all consmer
+             6. generate bill
+             7. view bill
+             8. Exit """)
+    choice =int(input("Enter your option : "))
+    if choice==1:
+        print("add consumer selected")
+        consumer_id = int(input("Enter the consumer id: "))
+        name = input("enter the name :")
+        phone = input("Enter the phone number : ")
+        place = input("Enter the address : ")
+        email = input("Enter the email id : ")
+        sql = "INSERT INTO `consumer`(`consumer_code`, `consumer_name`, `consumer_phone`, `consumer_email`, `consumer_adress`) VALUES (%s,%s,%s,%s,%s)"
+        data =(consumer_id,name,phone,place,email)
+        mycursor.execute(sql,data)
+        mydb.commit()
+    elif(choice==2):
+        print()
+    elif choice==8:
+        break
